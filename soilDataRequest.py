36e5dfb13222692e5f2c4b843277f9e1f942a89d
Neil Richard King Rice <kingrice@Neils-MacBook-Pro-2.local>
2020-11-28 17:18:37 -0500

    Updated output string to remove repeated values. Added in several test cases for coordinates all over the US.

--- soilDataRequest.py
+++ soilDataRequest.py
@@ -45,23 +45,45 @@ class soilDataRequest():
         
     def formatSoilDataString(self):
         
-        outputStr = ""
-        
         columnHeaders = ['Area Symbol', 'Area Name', 'Map Unit Symbol', 
                          'Map Unit Name', 'Map Unit Key', 'Soil Component Percent', 
                          'Soil Component Name']
         formattedTable = pandas.DataFrame(self.soilData['Table'], columns = columnHeaders)
-        for header in columnHeaders:
-            outputStr = (outputStr + header + ',' 
-                         + ','.join(list(formattedTable[header].values)) + ';')
+        
+        outputStr = ("Returned Values," + str(len(formattedTable['Soil Component Name']))
+                     + ";" )
+        
+        if len(pandas.unique(formattedTable['Map Unit Symbol']))==1:
+            for header in columnHeaders:
+                if header=='Soil Component Percent' or header=='Soil Component Name':
+                    outputStr = (outputStr + header + ','
+                                 + ','.join(list(formattedTable[header].values)) + ';')
+                elif header!='Area Symbol' and header!='Map Unit Key':
+                    outputStr = (outputStr + header + ','
+                                 + formattedTable[header][1] + ';')
+            
+        else :   
+            for header in columnHeaders:
+                outputStr = (outputStr + header + ','
+                             + ','.join(list(formattedTable[header].values)) + ';')
                 
         self.dataStr = bytes(outputStr, 'utf8')
             
 
 if __name__ == "__main__":
 
-    sdr = soilDataRequest(-76.8, 38.9)
-    sdr.submitRequest()
-    sdr.formatSoilDataString()
+    #sdr = soilDataRequest(-76.8, 38.9)
+    #sdr.submitRequest()
+    #sdr.formatSoilDataString()
     
-    print(sdr.dataStr)
+    #print(sdr.dataStr)
+    randomCoordinates = [[-77.380574, 38.790458],[-79.883852, 39.574043], 
+                         [-107.146538, 36.883092],[-96.099318, 35.922848],
+                         [-77.098347, 39.023427],[-77.037128, 38.923906]]
+    for coords in randomCoordinates:
+        sdr = soilDataRequest(coords[0], coords[1])
+        sdr.submitRequest()
+        sdr.formatSoilDataString()
+    
+        print(sdr.dataStr)
+        print('\n\n')
